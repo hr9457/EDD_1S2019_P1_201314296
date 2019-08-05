137dea1336c5cf2ce49f5b9a44298f5270bbd64d
Hector <hr9457@gmail.com>
2019-08-04 22:42:28 -0600

    pila, con metodo de agregar para la posicion de los scores

---
+++ pila.py
@@ -0,0 +1,61 @@
+# clase nodo
+# estructura del nodo
+class NodoPila:
+    # metodo contructor
+    def __init__(self,posX,posY):
+        self.posX = posX
+        self.posY = posY
+        self.siguientePila = None
+
+# clase pila
+# estrucura como se guardar los datos - pila
+class pila:
+    # metodo contructo
+    def __init__(self):
+        self.primeroPila = None
+        self.ultimoPila = None
+        self.sizePila = 1
+
+    # metodo para insertar en la pila
+    def Pop(self,posX,posY):
+        if self.ultimoPila == None:
+            nuevoNodo = NodoPila(posX,posY)
+            self.primeroPila = nuevoNodo
+            self.ultimoPila = nuevoNodo
+            self.sizePila += 1
+        else:
+            nuevoNodo = NodoPila(posX,posY)
+            self.ultimoPila.siguientePila = nuevoNodo
+            self.ultimoPila = nuevoNodo
+            self.sizePila += 1
+
+    # metodo para retornar el tamanio de la pila
+    def getSizePila(self):
+        return self.sizePila
+
+    def printPila(self):
+        temporalSize = self.sizePila
+        primerElemento = self.primeroPila
+        while temporalSize > 1:
+            temporalSize -= 1
+            print(primerElemento.posY)
+            primerElemento = primerElemento.siguientePila
+            
+
+
+''''
+# clse de arranque
+# prueba de pila
+if __name__ == "__main__":
+    p = pila()
+    
+    p.Pop(1,1)
+    p.Pop(1,2)
+    p.Pop(1,3)
+    print("elementos en la pila: "+  str(p.getSizePila())  )
+    p.printPila()
+''''
+
+
+
+        
