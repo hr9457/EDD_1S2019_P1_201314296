207e933d1e9a0c177862642b9adbab98155cba3f
Hector <hr9457@gmail.com>
2019-08-14 18:01:17 -0600

    busqueda archivos, aun no lo carga

--- cargaArchivo.py
+++ cargaArchivo.py
@@ -4,34 +4,29 @@ import curses #import the curses library
 import curses.textpad
 import time
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN #import special KEYS from the curses library
+import os
+
+def lecturaArchivo(ventana,nombreArchivo):
+    archivo = str(nombreArchivo).replace("b'","")
+    try:
+        f = open(""+archivo.replace("'","")+".csv","r")
+        ventana.addstr(10,10,"archivo cargado")
+    except:
+        ventana.addstr(10,10,"el archvio no exite : "+str(archivo).replace("'",""))
+    ventana.refresh()
 
 def carga():
     screen = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
+    curses.echo()#habila la escritura en la pantalla
+    curses.curs_set(1)
     numFilas , numColum = screen.getmaxyx()
+    #usuario = screen.getstr()
     ventana = curses.newwin(numFilas, numColum, 0, 0)
-    ventana.border(0)
-    ventana.keypad(True)
-    ventana.addstr(5,5," ingreses nombre del archivo para cargar:  ")
-    tb = curses.textpad.Textbox(ventana, insert_mode=True)
-    text = tb.edit()
-    #text = ventana.edit()
-    #tb = curses.textpad.Textbox(ventana)
-    #text = tb.edit()
-    curses.beep()
+    ventana.addstr(5,5,"Ingres nombre del archivo : ")
     ventana.keypad(1)
-    ventana.refresh()
-    ventana.timeout(-1) # mantiene la ventan indefinidadmente mientra se utilize el comando getch() para una entrada en pantalls
- 
-    #matiene abierto mientras teclas sean diferentes a la teclas esc
-    #teclado = ventana.getch()
-    teclado = int(text)
-    while teclado !=27:
-        text = tb.edit()
-        teclado = int(text)
-        #teclado = ventana.getch()
-        #teclado = -1
-        #teclado.refresh()
-
+    nombreArchivo = ventana.getstr()
+    curses.noecho()#desabilito la entrada por pantalla
+    curses.curs_set(0)
+    lecturaArchivo(ventana,nombreArchivo)
+    curses.napms(2000)
     curses.endwin()
